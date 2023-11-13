d9fe1e417d97bca596885a166500cd87ad61d2aa
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2023-11-13 06:29:07 +0530

    completed the video player

--- src/components/utils/VideoPlayer.jsx
+++ src/components/utils/VideoPlayer.jsx
@@ -9,19 +9,21 @@ import speak from '../../assets/speake.svg'
 import speaker from '../../assets/volume 100.svg'
 import speaker1 from '../../assets/volume 25.svg'
 import speaker2 from '../../assets/volume 50.svg'
-
-
+import speedicon from '../../assets/speedometer.svg'
+import full from '../../assets/full-screen-svgrepo-com 1.svg'
+import subt from '../../assets/subtitle.svg'
 const VideoPlayer = () => {
   const videoRef = useRef(null);
   const timelineRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const [volume, setVolume] = useState(1);
-  const [duration, setDuration] = useState(0);
+  const [duration, setDuration] = useState();
   const [currentTime, setCurrentTime] = useState(0);
   const [progress,setProgress]=useState(0)
-
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [subtitleTrack, setSubtitleTrack] = useState(false);
   const togglePlayPause = () => {
     const video = videoRef.current;
     if (video.paused) {
@@ -60,9 +62,20 @@ const VideoPlayer = () => {
       setCurrentTime(video.currentTime);
       const progressPercent = (video.currentTime / video.duration) * 100;
       setProgress(progressPercent);
+      if(video.currentTime===video.duration)
+       setIsPlaying(false);
     };
-  
+    const handleDurationUpdate = () => {
+    setDuration(video.duration)
+    }
+    
     video.addEventListener('timeupdate', handleTimeUpdate);
+    video.addEventListener('loadedmetadata', handleDurationUpdate);
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      video.removeEventListener('timeupdate', handleTimeUpdate);
+      video.removeEventListener('keydown',handleKeyDown);
+    };
   }, []);
   const handleSkipClick = () => {
     const video = videoRef.current;
@@ -84,37 +97,93 @@ const VideoPlayer = () => {
     videoRef.current.currentTime = newTime;
   };
   
+    const toggleOpen = () => setIsOpen(!isOpen);
+  
+    const [playbackSpeed, setPlaybackSpeed] = useState(1);
 
-  return (
-    <div className={`videoContainer ${isPlaying ? '' : 'paused'}`}>
-      <img className="thumbnail-img" alt="Video Thumbnail" />
+  const handleSpeedChange = (speed) => {
+    const video = videoRef.current;
+    video.playbackRate = speed;
+    setPlaybackSpeed(speed);
+    toggleOpen();
+  };
+  const formatDuration = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = Math.floor(time % 60);
+    return `${hours > 0 ? `${hours}:` : ''}${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    
+  };
+    const menuItems = ['2', '1.75', '1.5', '1.25', '1', '0.75', '0.5', '0.25'];
+    const toggleFullScreen = () => {
+      const videoContainer = document.querySelector('.videoContainer');
       
-      <div className="videoCont">
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+        setIsFullScreen(false);
+      } else {
+        videoContainer.requestFullscreen();
+        setIsFullScreen(true);
+      }}
+      const handleKeyDown = (e) => {
+        const video = videoRef.current;
+    
+        switch (e.key) {
+          case 'ArrowRight':
+            e.preventDefault();
+            video.currentTime += 10;
+            break;
+          case 'ArrowLeft':
+            e.preventDefault();
+            video.currentTime -= 10;
+            break;
+          case ' ':
+            e.preventDefault();
+            togglePlayPause();
+            break;
+            case 'Escape':
+              e.preventDefault();
+              if (document.fullscreenElement) {
+                document.exitFullscreen();
+                setIsFullScreen(false);
+              }
+          default:
+            break;
+        }
+      };
+      const handleSubtitleChange = () => {
+        setSubtitleTrack(!subtitleTrack);
+      };
+  return (
+    <div className={`videoContainer ${isPlaying? '' : 'paused'} ${!isOpen?'':'paused'}`}>
+      <img className="thumbnail"/>
+      
+      <div className={`videoCont ${isFullScreen ? 'fullscreen' : ''}`}>
       <div className='contContainer'>
       <div className="durationContainer">
-            <div className="current-time">{formatDuration(currentTime)}</div>
+            <div>{formatDuration(currentTime)}</div>
             /
-            <div className="total-time">{formatDuration(duration)}</div>
+            <div>{formatDuration(duration)}</div>
           </div>
         <div className="timelimeContainer" ref={timelineRef} onMouseDown={handleTimelineMouseDown}>
           <div className="timeline">
           <div className="progress" style={{ width: `${progress}%` }}>
-            <div className="thumb-indicator"></div>
             </div>
           </div>
         </div>
         <div className="controls">
-<button className='play-pause-btn' onClick={handlePrevClick}>
+          <div className='leftControl'>
+<button onClick={handlePrevClick}>
 <img src={back10}/>
 </button>
-          <button className="play-pause-btn" onClick={togglePlayPause}>
+          <button onClick={togglePlayPause}>
             {isPlaying ? (
               <img src={pause} />
             ) : (
              <img src={play}/>
             )}
           </button>
-          <button className='play-pause-btn' onClick={handleSkipClick}>
+          <button onClick={handleSkipClick}>
 <img src={forw10}/>
 </button>
           <div className="volumeContainer">
@@ -137,20 +206,35 @@ const VideoPlayer = () => {
               onChange={handleVolumeChange}
             />
           </div>
+          </div>
+          <div className='leftControl'>
+      <button onClick={toggleOpen}>
+        <img src={speedicon}/></button>
+      {isOpen && (
+        <div className="menuItems">
+          {menuItems.map((item, index) => (
+            <div key={index} className={`menuItem ${item == playbackSpeed ? 'selected' : ''}`} onClick={() => handleSpeedChange(item)}>
+              {item}x
+            </div>
+          ))}
+        </div>
+      )}
+    <button onClick={handleSubtitleChange}><img className={`subt ${subtitleTrack ? 'subt1' : ''}`} src={subt}/></button>
+
+    <button onClick={toggleFullScreen}>{
+      <img src={full}/>}</button>
+    </div>
          </div>
         </div>
       </div>
       <video ref={videoRef} src={video}>
+      {subtitleTrack && (
+          <track
+          />
+        )}
       </video>
     </div>
   );
-};
-
-const formatDuration = (time) => {
-  const hours = Math.floor(time / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const seconds = Math.floor(time % 60);
-  return `${hours > 0 ? `${hours}:` : ''}${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 };
 
 export default VideoPlayer;
